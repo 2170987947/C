c316f190c3e938fdac66850cdf3b60f4fef81a62
2170987947 <2170987947@qq.com>
2020-11-14 23:46:56 +0800

    字符串左转

--- 2020_1113/2020_1113/test.c
+++ 2020_1113/2020_1113/test.c
@@ -1,32 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 //字符串左旋
 /*实现一个函数，可以左旋字符串中的k个字符。
 例如：
 ABCD左旋一个字符得到BCDA
 ABCD左旋两个字符得到CDAB*/
-void leftRotate(int* a,int num){
+void leftRotate(char* str,int num){
 	while (num>0){
-		//相互交换
-		for (; *a != '\0'; a++){
-
+		char* a = str;
+		char temp = *str;
+		for (; *(a+1) != '\0'; a++){
+			*a=*(a+1);
 		}
+		*a = temp;
 		num--;
 	}
 }
 
 int main()
 {
-	char a[] = "Hello";
+	char a[] = "ABCD";
 	//表示左旋多少个字符
 	int num;
 	printf("请输入左旋字符的个数:");
 	scanf("%d", &num);
+	leftRotate(a, num);
+	printf("%s\n", a);
 	//定义函数指针类型为Ptr
-	typedef void(*Ptr)(int,int);
+	/*typedef void(*Ptr)(char*,int);
 	//定义函数指针类型的变量
 	Ptr point;
 	//调用
-	point(a,num);   
+	point(a,num);   */
 	system("pause");
 	return 0;
 }
